15e939c8b0ae297ad19ccb50600d68a0a2ba8844
Reaver113 <jasonalcock113@gmail.com>
2024-11-13 12:02:52 +1000

    Nav animations Improved

--- src/app/components/Nav/Nav-Items.tsx
+++ src/app/components/Nav/Nav-Items.tsx
@@ -16,10 +16,10 @@ export default function NavItem({ item, isSelected, changeSelected }: navItemPro
 
 	return(
 		<div onClick={handleClick} className={`${styles.navItemContainer}  ${isSelected === item ? '' : 'desktop:hover:bg-[#C1121F] laptop:hover:bg-[#C1121F]'} desktop:mt-8 desktop:group-hover:mt-0 laptop:mt-8 laptop:group-hover:mt-0`}>
-			<div className={`${styles.navItemImage} ${isSelected === item ? styles.selected : ''} desktop:group-hover:h-24 desktop:group-hover:w-24 laptop:group-hover:h-24 laptop:group-hover:w-24`}>
+			<div className={`${styles.navItemImage} ${isSelected === item ? styles.selected : ''} delay-75 desktop:group-hover:h-24 desktop:group-hover:w-24 laptop:group-hover:h-24 laptop:group-hover:w-24`}>
 				<IconPicker iconName={item}/>
 			</div>
-			<div className={`${styles.navItemTextContainer} ${isSelected === item ? styles.selected : ''} desktop:group-hover:w-full laptop:group-hover:w-full`}>
+			<div id='itemText' className={`${styles.navItemTextContainer} ${isSelected === item ? styles.selected : ''} delay-75 desktop:group-hover:w-full laptop:group-hover:w-full`}>
 				<div className={`${styles.navItemName} flex`}>{item}</div>
 			</div>
 		</div>
