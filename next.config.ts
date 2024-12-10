8e6b8cbaaabe720e1682ff7d17ebfd0344c9b497
Reaver113 <jasonalcock113@gmail.com>
2024-12-10 18:19:34 +1000

    added page.tsx

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/portfolio-site",
   output: "export",
-  reactStrictMode: true,
   images: {
     unoptimized: true
   }
---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import { getCareerDoc, getInterestsDoc, getprofileDoc } from "./components/Shared/Fetch/FetchDoc";
+import ComponentSwitcher from "./components/Component Switcher/Component-Switcher";
+
+
+const profile = await getprofileDoc();
+const career = await getCareerDoc();
+const interests = await getInterestsDoc();
+
+export default function Home() {
+
+
+
+	const documents = [
+		profile,
+		career,
+		interests
+	]
+	
+  return <ComponentSwitcher {...documents} />
+}
